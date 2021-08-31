c0a4aeebbb1542c2477b3d25dedad1cbc1fb2fb4
Ulaş Can Demirbağ <ulascandemirbag@gmail.com>
2021-08-31 19:39:03 +0300

    header complate

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,28 @@
-console.log("hello worlds");
+const btnHamburger =document.getElementById('btnHamburger')
+const body=document.querySelector('body')
+const header=document.querySelector('.header')
+const overlay=document.querySelector('.overlay')
+const fadeElems=document.querySelectorAll('.has-fade')
+
+btnHamburger.addEventListener("click",()=>
+  {
+    if (header.classList.contains('open')) {//close
+      body.classList.remove('noscroll')
+      header.classList.remove('open')
+      fadeElems.forEach((element)=>{
+        element.classList.remove('fade-in')
+        element.classList.add('fade-out')
+      })
+
+    }
+    else {//open
+      body.classList.add('noscroll')
+      header.classList.add('open')
+      fadeElems.forEach((element)=>{
+        element.classList.remove('fade-out')
+        element.classList.add('fade-in')
+      })
+
+    }
+  }
+)
